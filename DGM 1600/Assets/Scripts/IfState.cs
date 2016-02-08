6a0a0e56141a4d2be5440ffc255c149244871b70
Linda Abigail Budd <abbybudd@Lindas-MBP.ad.uvu.edu>
2016-02-08 08:52:22 -0700

    My 4th file - added a minor feature

---
+++ DGM 1600/Assets/Scripts/IfState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfState : MonoBehaviour
+{
+	public float hotcocoaTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		hotcocoaTemperature -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest ()
+	{
+		if(hotcocoaTemperature > hotLimitTemperature)
+		{
+			Debug.Log("Hot Cocoa is too hot.");
+		}
+		else if(hotcocoaTemperature < coldLimitTemperature)
+		{
+			Debug.Log("Hot Cocoa is too cold.");
+		}
+		else
+		{
+			Debug.Log("Hot Cocoa is just right.");
+		}
+	}
+}
---
+++ DGM 1600/Assets/Scripts/VariandFunc.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariandFunc : MonoBehaviour 
+{
+	int myInt = 2;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = DivideByTwo (myInt);
+		Debug.Log (myInt);
+	}
+
+	int DivideByTwo (int number)
+	{
+		int ret;
+		ret = number / 2;
+		return ret;
+	}
+}
